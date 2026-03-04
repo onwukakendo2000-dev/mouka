0ae42710474e9d35da840a0acbd404f5a29da4b0
Onwuka Kenechukwu <onwukakendo2000@gmail.com>
2026-03-04 11:05:57 +0100

    fixed ui layout and sidebar toggling

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
-console.log('Testing JavaScript');
+// Handle sidebar toggle
+function showSidebar(){
+    const sidebar = document.querySelector('.sidebar-wrapper')
+    sidebar.style.display = 'block';
+}
+
+function hideSidebar(){
+    const sidebar = document.querySelector('.sidebar-wrapper')
+    sidebar.style.display = 'none'
+}
+
+// Page Scroll animation
+const obeserver = new IntersectionObserver((entries) => {
+    entries.forEach((e) => {
+        if(e.isIntersecting){
+            e.target.classList.add('visible');
+        } 
+    });
+}, { threshold: 0.2 });
+
+const fadeElements = document.querySelectorAll('.fade-in');
+fadeElements.forEach((el) => { obeserver.observe(el); });
